61edbc7af77cb698d4e941eccfce0c7313de7d48
Aishwarya <aishwarya.verma28@gmail.com>
2022-11-27 18:44:52 +0530

    dom test

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
 const options=document.querySelector(".options");
+console.log(options.value);
 const container=document.querySelector(".container");
+const buttonDiv=document.querySelector(".buttons");
+const all=document.querySelector(".all");
+const headphone=document.querySelector(".headph");
+const laptop=document.querySelector(".laptop");
+const mobile=document.querySelector(".mobile");
 
 //EVENT Listener
 document.querySelector("form").addEventListener("submit", appendItem);
@@ -14,6 +20,7 @@ function appendItem(event){
     else{
     let div=document.createElement("div");
     div.setAttribute("class","item")
+    div.classList.add("all");
     const url=document.querySelector("#url").value;
     let image=document.createElement("img");
     image.src=url;
@@ -25,6 +32,13 @@ function appendItem(event){
     div.append(image, btn);
     container.append(div);
     document.querySelector("#url").value="";
+    if(options.value=="headphone")
+    div.classList.add("headphone");
+    if(options.value=="mobile")
+    div.classList.add("mobile");
+    if(options.value=="laptop")
+    div.classList.add("laptop");
+    console.log(container.innerHTML);
 }
 }
 function deleteDiv(e){
@@ -35,4 +49,36 @@ function deleteDiv(e){
     delBtn.parentElement.remove();
 
 }
+
+all.addEventListener("click",()=>{
+    for(let i=0; i<all.length; i++)
+    all[i].classList.remove("display_none");
+
+})
+headphone.addEventListener("click",()=>{
+    for(let i=0;i<all.length;i++)
+    {
+        laptop[i].classList.add("display_none");
+        mobile[i].classList.add("display_none");
+        headphone[i].classList.remove("display_none");
+    }
+})
+
+laptop.addEventListener("click",()=>{
+    for(let i=0;i<all.length;i++)
+    {
+        headphone[i].classList.add("display_none");
+        mobile[i].classList.add("display_none");
+        laptop[i].classList.remove("display_none");
+    }
+})
+
+mobile.addEventListener("click",()=>{
+    for(let i=0;i<all.length;i++)
+    {
+        headphone[i].classList.add("display_none");
+        laptop[i].classList.add("display_none");
+        mobile[i].classList.remove("display_none");
+    }
+})
 
